c4a1c9a30da38fbb0ce8b9903de3e4c32c521b97
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-08-22 01:16:28 +0300

    Add new functionallity

--- git.sh
+++ git.sh
@@ -1,29 +1,144 @@
 #!/bin/bash
 
+# Script developed by Sergey Zhyltsov
+#This script automates the project building process for microservices on your local machine, including checking and installing required software programs . Creating necessary project directories with pulling code repositories
+ 
+# List of required programs. Add new program name here for installing software 
+# IMPORTANT: Do not forget added new condition to the InstallProgram function 
+programs=("git" "ssh" "openvpn")
+
+# Workdir where's project location
+project_dir="$HOME/Документы/MusicBox"
+
+# Base URL for repositories
+repo_base_url="https://gitlab.musicbox.local/music-box"
+
+# Map of project directories relative to the project_dir and their corresponding repository URLs
 declare -A directory_repository_map=(
-                   ["$HOME/Документы/MusicBox/api/auth"]="https://gitlab.musicbox.local/music-box/back-end/auth"
-                  ["$HOME/Документы/MusicBox/api/admin"]="https://gitlab.musicbox.local/music-box/back-end/admin"
-                 ["$HOME/Документы/MusicBox/api/public"]="https://gitlab.musicbox.local/music-box/back-end/public"
-                ["$HOME/Документы/MusicBox/consumer"]="https://gitlab.musicbox.local/music-box/back-end/mail"
-               ["$HOME/Документы/MusicBox/frontend/admin"]="https://gitlab.musicbox.local/music-box/front-end/admin"
-              ["$HOME/Документы/MusicBox/frontend/public"]="https://gitlab.musicbox.local/music-box/front-end/public"
+    		  ["$project_dir/api/auth"]="$repo_base_url/back-end/auth"
+   		  ["$project_dir/api/admin"]="$repo_base_url/back-end/admin"
+   		  ["$project_dir/api/public"]="$repo_base_url/back-end/public"
+   		  ["$project_dir/consumer"]="$repo_base_url/back-end/mail"
+    		  ["$project_dir/frontend/admin"]="$repo_base_url/front-end/admin"
+    		  ["$project_dir/frontend/public"]="$repo_base_url/front-end/public"
 )
 
-for dir in "${!directory_repository_map[@]}"; do
-            if [   -d   "$dir"   ]; 
-            then
-                   cd  $dir
-                      #git init
-                      #git remote add origin ${directory_repository_map[$dir]}
-                      #git pull       
-           else 
-                  echo "Project directory isn't exist ..."
-                  echo "Create directory: $dir" 
-             mkdir -p $dir
-             echo "Directory create successfull"
-          fi        
-    echo
-done
+#Config access to gitlab 
+function SetAccessConfig()
+{
+       local answer=""
+    while true; do
+        echo "Do you have an SSH access key? [y/n]"
+        read -r answer 
 
-unset directory_repository_map
+        if [[ "$answer" == "y" || "$answer" == "Y"  || "$answer" == "yes"]]; then       
+            eval "$(ssh-agent -s)"
+               ssh-add ~/.ssh/id_rsa
+            break
+        elif [[ "$answer" == "n" || "$answer" == "N" || "$answer" == "no"]]; then
+                 local inbox=""
+                     echo "Entry GitLab inbox name: " 
+                        read -n $inbox
+                    ssh-keygen -t rsa -b 4096 -C $inbox
+                        eval "$(ssh-agent -s)"
+                  ssh-add ~/.ssh/id_rsa
+            break
+        else
+            echo "Invalid input. Please enter 'y' or 'n'."
+        fi
+    done
+}
+
+#Installation needed utilities.
+#So when we added new name to the lists of required programs , you need to add new condition here
+function InstallProgram()
+{
+    local program="$1"
+    case "$program" in
+        git)
+            echo "Installing $program..."
+            sudo apt install git -y
+            ;;
+        ssh)
+            echo "Installing $program..."
+             sudo apt install ssh
+             ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+             sudo systemctl enable sshd
+            
+            ;;
+        openvpn)
+            echo "Installing $program..."
+            sudo apt install openvpn -y
+            ;;
+        *)
+            echo "Unknown program: $program"
+            ;;
+    esac
+}
+
+#Check software installation 
+function CheckSoftwareInstallation()
+{ 
+  local results=" "
+     for program in "${programs[@]}"; do
+    	   if which "$program" > /dev/null; then
+        	   results+="[$program: Success] "
+        	#return 0
+    	   else
+        	#results+="[$program: Failure] "
+        	InstallProgram "$program"
+    	   fi
+      done
+echo "Check software installation: $results " 				 
+}
+
+# Create Project Directory and return true if successful, false otherwise
+function CreateFolder()
+{
+    local directory="$1"
+    if [ -d "$directory" ]; then
+           return 0 # Success
+    else
+           mkdir -p "$directory"
+        #return 1 # Failure
+    fi     
+}
+
+# Pulls changes from the current repository
+function GitPullChanges()
+{
+    local repoLinked="$1"
+    rm -rf .git
+    	git init
+    	      git config --global init.defaultBranch main
+    	      git config --global http.sslverify false
+    	      git branch -M main
+    	      git remote add origin "$repoLinked"
+    	git pull origin main
+}
+
+
+function Start()
+{
+    CheckSoftwareInstallation
+    local installation_result=$?
+    if [ "$installation_result" -eq 0 ]; then
+        for dir in "${!directory_repository_map[@]}"; do
+            if CreateFolder "$dir"; then
+                 cd "$dir"
+                 GitPullChanges "${directory_repository_map[$dir]}"  
+            else
+                 echo "The directory $dir doesn't exist and will be created ..."
+                 ls "$dir"
+            fi        
+        done
+        echo "Search project directory ...  $directory"  
+    else
+        echo "Software installation failed, cannot continue."
+    fi
+       unset directory_repository_map
+    unset programs 
+}
+# Running
+Start
 
